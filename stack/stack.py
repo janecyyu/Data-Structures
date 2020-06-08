dfb6656b8b34cfad8883df5d3638603ab07105c2
janecyyu <janecyyu@gmail.com>
2020-06-08 15:37:41 -0700

    stack linkedlist ok

--- stack/stack.py
+++ stack/stack.py
@@ -12,31 +12,46 @@ return elements in Last In First Out order.
 """
 
 # Array version:
-my_array = [1, 2, 3, 4, 5, 6]
+
+
+# class Stack:
+#     def __init__(self):
+#         self.storage = []
+#         self.size = len((self.storage))
+
+#     def __len__(self):
+#         return(len((self.storage)))
+
+#     def push(self, value):
+#         self.storage.insert(0, value)
+#         return self.storage
+
+#     def pop(self):
+#         if len((self.storage)) == 0:
+#             return None
+#         else:
+#             rm_data = self.storage.pop(0)
+#             return rm_data
+
+# Linked List version:
+from singly_linked_list import LinkedList
 
 
 class Stack:
     def __init__(self):
-        self.storage = []
-        self.size = len((self.storage))
+        self.storage = LinkedList()
+        self.size = self.storage.getCount()
 
     def __len__(self):
-        return(len((self.storage)))
+        return(self.storage.getCount())
 
     def push(self, value):
-        self.storage.insert(0, value)
+        self.storage.add_to_tail(value)
         return self.storage
 
     def pop(self):
-        if len((self.storage)) == 0:
+        if self.storage.getCount() == 0:
             return None
         else:
-            rm_data = self.storage.pop(0)
+            rm_data = self.storage.remove_tail()
             return rm_data
-
-
-arr_stack = Stack()
-arr_stack.storage = my_array
-print(arr_stack.__len__())
-print(arr_stack.push(7))
-print(arr_stack.pop())
